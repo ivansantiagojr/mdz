33f88264a1383a5dc1f88c245c7e2d815d604641
ivansantiagojr <ivansantiago.junior@gmail.com>
2025-06-28 19:10:05 -0300

    Just testing my ziggy pydust fork

--- src/hello.zig
+++ src/hello.zig
@@ -1,9 +1,36 @@
 const py = @import("pydust");
+const std = @import("std");
+
+const Zmd = @import("zmd").Zmd;
+const fragments = @import("zmd").html.DefaultFragments;
 
 const root = @This();
 
 pub fn hello() !py.PyString(root) {
-    return try py.PyString(root).create("Hello!");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var zmd = Zmd.init(allocator);
+    defer zmd.deinit();
+
+    try zmd.parse(
+        \\# Header
+        \\## Sub-header
+        \\### Sub-sub-header
+        \\
+        \\some text in **bold** and _italic_
+        \\
+        \\a paragraph
+        \\
+        \\```zig
+        \\some code
+        \\```
+        \\some more text with a `code` fragment
+    );
+
+    const html = try zmd.toHtml(fragments);
+
+    return py.PyString(root).create(html);
 }
 
 comptime {
